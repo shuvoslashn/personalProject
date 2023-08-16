328fe306cb37c5620377b7cde1e407d9e85633ab
Shuvo Sarker <sarker3664@gmail.com>
2023-08-17 03:15:05 +0600

    feat - frame created

--- frontEnd/react/smovie-lite/src/context/context.jsx
+++ frontEnd/react/smovie-lite/src/context/context.jsx
@@ -1,15 +1,28 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 const API_URL = `http://www.omdbapi.com/?apikey=f3a11812&s=titanic`;
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+    const [isError, setIsError] = useState({ show: 'false', msg: '' });
+
     const getMovies = async (url) => {
         try {
             const res = await fetch(url);
             const data = await res.json();
             console.log(data);
+            if (data.Response === 'True') {
+                setIsLoading(false);
+                setMovie(data.Search);
+            } else {
+                setIsError({
+                    show: true,
+                    msg: data.error,
+                });
+            }
         } catch (error) {
             console.log(error);
         }
