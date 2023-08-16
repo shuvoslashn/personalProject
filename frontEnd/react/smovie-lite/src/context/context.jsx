b5cc4a81a90b64bdeeeec807cf17f976c70a4f33
Shuvo Sarker <sarker3664@gmail.com>
2023-08-16 23:53:51 +0600

    feat - data get successful using fetch api

--- frontEnd/react/smovie-lite/src/context/context.jsx
+++ frontEnd/react/smovie-lite/src/context/context.jsx
@@ -1,8 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+
+const API_URL = `http://www.omdbapi.com/?apikey=f3a11812&s=titanic`;
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+    const getMovies = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getMovies(API_URL);
+    }, []);
     return <AppContext.Provider value={'Shuvo'}>{children}</AppContext.Provider>;
 };
 
