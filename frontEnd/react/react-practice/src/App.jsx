718a2768457f61c29a20343d1d1c83f8bc39c53f
Shuvo Sarker <sarker3664@gmail.com>
2023-08-18 23:07:19 +0600

    learn conditional renderting

--- frontEnd/react/react-practice/src/App.jsx
+++ frontEnd/react/react-practice/src/App.jsx
@@ -4,9 +4,19 @@ import { dataset } from './data/dataset';
 
 const App = () => {
     return (
-        <div className='flex flex-wrap'>
+        <div className='flex flex-wrap m-5'>
             {dataset.map((e, i) => {
-                return <Video key={i} title={e.title} des={e.des} views={e.views} />;
+                return (
+                    <Video
+                        key={i}
+                        title={e.title}
+                        des={e.des}
+                        views={e.views}
+                        channel={e.channel}
+                        verified={e.verified}
+                        id={i}
+                    />
+                );
             })}
         </div>
     );
--- frontEnd/react/react-practice/src/components/Video.jsx
+++ frontEnd/react/react-practice/src/components/Video.jsx
@@ -1,13 +1,19 @@
-const Video = ({ title, des, views }) => {
-    const img = `https://picsum.photos/id/900/1800/500`;
+const Video = ({ title, des, views, channel, verified, id }) => {
+    const img = `https://picsum.photos/id/6${id}/1800/800`;
     return (
-        <div className='flex flex-col basis-96 flex-grow'>
+        <div className='flex flex-col basis-1/4 flex-grow'>
             <img src={img} alt='' className='block m-4 mb-0' />
-            <div className='m-4 mt-0 p-4 bg-gray-800'>
-                <h1 className='text-3xl capitalize font-bold pb-2 text-gray-300'>{title}</h1>
-                <p>
-                    {des} . {views}
-                </p>
+            <div className='m-4 mt-0 p-8 bg-gray-800'>
+                <h1 className='text-2xl capitalize font-bold pb-2 text-gray-300'>{title}</h1>
+                <div className='flex justify-between'>
+                    <p className='text-xl font-bold text-gray-400'>
+                        {channel}
+                        <span className='ml-2'>{verified ? <i className='ri-shield-check-fill' /> : null}</span>
+                    </p>
+                    <p className='text-xl'>
+                        {des} . {views}
+                    </p>
+                </div>
             </div>
         </div>
     );
