ea10de28f7ff951f4f3314043d6c12377c1afc0f
Shuvo Sarker <sarker3664@gmail.com>
2023-08-18 21:51:27 +0600

    Learned export & import inDepth

--- frontEnd/react/react-practice/src/components/Video.jsx
+++ frontEnd/react/react-practice/src/components/Video.jsx
@@ -2,6 +2,7 @@ const Video = () => {
     return (
         <div>
             <h1>Video</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis perspiciatis rem, iste, minus et architecto labore asperiores sit delectus dolorem corrupti velit quae illo, assumenda nisi magnam laudantium. Veniam, aperiam!</p>
         </div>
     );
 };
