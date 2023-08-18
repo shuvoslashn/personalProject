e5564779e25f1eaca67c86e329c628bc10b7aaea
Shuvo Sarker <sarker3664@gmail.com>
2023-08-18 22:03:07 +0600

    learn how to use props in jsx

--- frontEnd/react/react-practice/src/components/Video.jsx
+++ frontEnd/react/react-practice/src/components/Video.jsx
@@ -1,8 +1,12 @@
 const Video = ({ title, des }) => {
+    const img = `https://picsum.photos/id/900/1800/500`;
     return (
-        <div>
-            <h1>{title}</h1>
-            <p>{des}</p>
+        <div className='flex flex-col'>
+            <img src={img} alt='' className='block m-4 mb-0' />
+            <div className='m-4 mt-0 p-4 bg-gray-800'>
+                <h1 className='text-3xl capitalize font-bold pb-2 text-gray-300'>{title}</h1>
+                <p>{des}</p>
+            </div>
         </div>
     );
 };
