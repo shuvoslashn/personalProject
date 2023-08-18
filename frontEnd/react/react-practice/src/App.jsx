fa734fb9aac590386b2e3d47b8ff5cc1bb4ed609
Shuvo Sarker <sarker3664@gmail.com>
2023-08-18 20:42:19 +0600

    start practicing react js

---
+++ frontEnd/react/react-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Video from './components/Video';
+
+const App = () => {
+    return (
+        <div>
+            <Video />
+        </div>
+    );
+};
+
+export default App;
