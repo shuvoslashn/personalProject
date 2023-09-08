7ca4951fb0853c34df44ae75213b296db257125e
Shuvo Sarker <sarker3664@gmail.com>
2023-09-08 22:32:47 +0600

    initial setup done

---
+++ frontEnd/react-redux/counter/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className='text-2xl p-4 text-center bg-gray-900 text-white'>React & Redux</h1>
+        </div>
+    );
+};
+
+export default App;
